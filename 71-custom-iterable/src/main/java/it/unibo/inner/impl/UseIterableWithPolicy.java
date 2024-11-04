cd0ce10a8e12293328dbb5138818e3721bd75441
Emanuele Fabricat - emanuele.fabricat@studio.unibo.it <emanuele.fabricat@studio.unibo.it>
2024-11-04 11:59:54 +0100

    make inner class, implements iterator in UseIterableWithPolicy. implements getIterableWithPolicy in TestIterablePlain es 71

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/UseIterableWithPolicy.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/UseIterableWithPolicy.java
@@ -1,5 +1,42 @@
 package it.unibo.inner.impl;
 
-public class UseIterableWithPolicy {
+import java.util.Iterator;
+
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+
+public class UseIterableWithPolicy<T> implements IterableWithPolicy<T>{
+    private T[] array;
+
+    public UseIterableWithPolicy(T[] array){
+        this.array = array;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new MyIterator();
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate filter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+    }
+
+    public class MyIterator implements Iterator<T>{
+        @Override
+        public boolean hasNext() {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'hasNext'");
+        }
+
+        @Override
+        public T next() {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'next'");
+        }
+    
+        
+    }
 
 }

e6b7630c8f7f7b951cb77b2ec28719f31014634d
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2024-11-08 16:08:04 +0100

    insert some fuction to Function and Month Sorter Nested

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -11,21 +11,22 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new SortByDate();
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new SortByMonthOrder();
     }
 
     public enum Month {
-        JENUARY("Jenuary", 31),
-        FEBRUARY("Fabruary", 28),
-        MARCH("March", 31),
-        APRIL("April", 30),
-        JUNE("June", 31),
-        JULY("July", 30),
+        JENUARY("january", 31),
+        FEBRUARY("february", 28),
+        MARCH("march", 31),
+        APRIL("april", 30),
+        MAY("may", 31),
+        JUNE("june", 31),
+        JULY("july", 30),
         AUGUST("august", 31),
         SEPTEMBER("september", 30),
         OCTOBER("october", 31),
@@ -41,16 +42,16 @@ public final class MonthSorterNested implements MonthSorter {
         };  
 
         public String getName() {
-            return name;
+            return this.name;
         }
         public int getDays() {
-            return days;
+            return this.days;
         }
         public Month fromString(String text){
             int cont = 0;
             Month result = null;
             for(Month month: Month.values()){
-                if(month.getName().startsWith(text)){
+                if(month.getName().startsWith(text.toLowerCase())){
                     result = month;
                     cont ++;
                 }
@@ -58,7 +59,47 @@ public final class MonthSorterNested implements MonthSorter {
             if(cont == 1){
                 return result;
             }else{
-                throw new IllegalArgumentException("Month not found or empty field");
+                throw new IllegalArgumentException("Month not found or empty field" + text);
+            }
+        }
+    }
+    private class SortByMonthOrder implements Comparator<String>{
+
+        @Override
+        public int compare(String o1, String o2) {
+            for(MonthSorterNested.Month month: MonthSorterNested.Month.values()){
+                if(month.fromString(o1) instanceof MonthSorterNested.Month){
+                    return -1;
+                }else if(month.fromString(o2) instanceof MonthSorterNested.Month){
+                    return 1;
+                }
+            }
+            throw new IllegalArgumentException("It's impossibile to compare");
+        }
+        
+    }
+    private class SortByDate implements Comparator<String> {
+        
+        @Override
+        public int compare(String o1, String o2) {
+            int numb1 = 0;
+            int numb2 = 0;
+            for(MonthSorterNested.Month month: MonthSorterNested.Month.values()){
+                if(month.getName().equals(o1)){
+                    numb1 = month.getDays();
+                }else if(month.getName().equals(o2)){
+                    numb2 = month.getDays();
+                }
+            }
+            if(numb1 == 0 || numb2 == 0){
+                throw new IllegalArgumentException("impossible to compare" + o1);
+            }
+            if(numb1 < numb2){
+                return -1;
+            }else if(numb1 > numb2){
+                return 1;
+            }else{
+                return 0;
             }
         }
     }
